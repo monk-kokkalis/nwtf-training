cfd2a56013612e3dc0c1692348bc26654687418a
Will Yngson <christianwillyngson@gmail.com>
2022-02-06 16:48:42 +0800

    initial commit

---
+++ src/app/App.js
@@ -0,0 +1,11 @@
+import './App.module.scss';
+import Style from './App.module.scss';
+function App() {
+    return (
+        <div className={Style.Main}>
+            <div>fat app</div>
+        </div>
+    );
+}
+
+export default App;
