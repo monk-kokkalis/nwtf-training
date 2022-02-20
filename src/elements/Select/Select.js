d47ff4e9c409c1291bebea6e1e362b17bc2e772a
Will Yngson <christianwillyngson@gmail.com>
2022-02-21 02:05:10 +0800

    added Select element

---
+++ src/elements/Select/Select.js
@@ -0,0 +1,38 @@
+import {
+    FormControl,
+    InputLabel,
+    MenuItem,
+    Select as MaterialSelect
+} from '@mui/material';
+function Select(props) {
+    return (
+        <FormControl fullWidth size="small">
+            {props.label ? <InputLabel>{props.label}</InputLabel> : undefined}
+            <MaterialSelect
+                label={props.label}
+                MenuProps={{
+                    anchorOrigin:{
+                        vertical: 'bottom',
+                        horizontal: 'left',
+                    },
+                    transformOrigin: {
+                        vertical: 'center',
+                        horizontal: 'left',
+                    },
+                    PaperProps: {
+                        style: {
+                            maxHeight: '200px',
+                        }
+                    }
+                }}
+                onChange={props.onChange}
+                value={props.value}>
+                {props.options?.map((op, index) => (
+                    <MenuItem key={index} value={op.value}>{op.label}</MenuItem>
+                ))}                
+            </MaterialSelect>
+        </FormControl>
+    )
+}
+
+export default Select;
