8e401f444abc663124f32438e17f8c600f6c9fbe
Will Yngson <christianwillyngson@gmail.com>
2022-02-21 00:11:11 +0800

    added TextField component and additional mixins

---
+++ src/elements/TextField/TextField.js
@@ -0,0 +1,14 @@
+import {TextField as MaterialTextField} from '@mui/material';
+
+function TextField(props) {
+    return (
+        <MaterialTextField
+            fullWidth={true}
+            label={props.label || 'Label'}
+            variant={props.variant || "outlined"}
+            size={props.size || "small"}
+        />   
+    )
+}
+
+export default TextField
