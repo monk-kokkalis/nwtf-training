b7d460e8d000de006f5c9019ea9ee603c8fda1ab
Will Yngson <christianwillyngson@gmail.com>
2022-02-21 02:06:12 +0800

    added option data

---
+++ src/pages/Register/components/data/options/days.js
@@ -0,0 +1,5 @@
+const days = [...Array(31).keys()].map(el => (
+    {label: el + 1, value: el + 1}
+))
+
+export default days;
---
+++ src/pages/Register/components/data/options/years.js
@@ -0,0 +1,7 @@
+const years = [];
+
+for (let number = 2000; number <= 2020; number++) {
+    years.push({label: number, value: number})
+}
+
+export default years;
