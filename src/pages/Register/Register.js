f739269dbbe0f417165fcd9573ebd5f65ad39df6
Will Yngson <christianwillyngson@gmail.com>
2022-02-21 00:12:27 +0800

    updated Register template

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,7 +1,35 @@
+import Style from './Register.module.scss';
+import {Link} from "react-router-dom";
+import logo from 'assets/logo.svg';
+// content
+import PersonalDetails from './components/PersonalDetails';
+// elements
+import Wizard from 'elements/Wizard/Wizard';
 function Register() {
     return (
-        <div>
-            Register page here
+        <div className={Style.Main}>
+            <img src={logo} className="logo" alt="logo" />
+            <h1>Register</h1>
+            <h2>
+                <span>Already have an account?</span>
+                <Link to="/login" className='login--link'>Sign in</Link>
+            </h2>
+            <section className="wizard">
+                <Wizard
+                    steps={[
+                        'Personal details',
+                        'Address',
+                        'Contact'
+                    ]}
+                    content={
+                        [
+                            <PersonalDetails />,
+                            <div>fat second</div>,
+                            <div>fat third</div>
+                        ]
+                    }
+                />
+            </section>
         </div>
     )
 }
