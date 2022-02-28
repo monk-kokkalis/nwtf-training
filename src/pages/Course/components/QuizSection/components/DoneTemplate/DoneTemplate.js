75dc1b7e26ecdb012ada9be9a148d1a4feb34c4c
Will Yngson <christianwillyngson@gmail.com>
2022-02-28 16:02:21 +0800

    added overview to DoneTemplate

--- src/pages/Course/components/QuizSection/components/DoneTemplate/DoneTemplate.js
+++ src/pages/Course/components/QuizSection/components/DoneTemplate/DoneTemplate.js
@@ -7,7 +7,27 @@ function DoneTemplate() {
     const context = useContext(Context);
     return (
         <div className={Style.Main}>
-            <CircularDeterminate value={80} color="green" subtitle="Passed" />
+            <section className="overview">
+                <CircularDeterminate value={80} color="green" subtitle="Passed" />
+                <div className="data">
+                    <div className="row">
+                        <div className="key">Date</div>
+                        <div className="value">February 28, 2022</div>
+                    </div>
+                    <div className="row">
+                        <div className="key">Answered</div>
+                        <div className="value">10/10</div>
+                    </div>
+                    <div className="row">
+                        <div className="key">Passing score</div>
+                        <div className="value">8 (80%)</div>
+                    </div>
+                    <div className="row">
+                        <div className="key">Score</div>
+                        <div className="value">8 / 10 (80%)</div>
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
